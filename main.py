731e0e9de377de1da32050b3452091626b0d9c6f
Laurens Koppenol <lt.koppenol@portofrotterdam.com>
2021-02-28 17:42:20 +0100

    create train- test- valid-datasets

--- main.py
+++ main.py
@@ -2,10 +2,22 @@ from rimworld import utils as rimworld_utils
 from pathlib import Path
 
 
-def create_test_dataset():
-    test_path = Path("./data/nsynth-test/")
-    rimworld_utils.create_spectrum_dataset(test_path, n_fft=2048)
+def create_spectrum_dataset(data_type):
+    assert data_type in ['test', 'train', 'valid']
+    data_folder = Path(f"./data/nsynth-{data_type}/")
+
+    lekkere_columns = ['instrument', 'pitch', 'velocity']
+    metadata = rimworld_utils \
+        .read_metadata(data_folder) \
+        [lekkere_columns]
+
+    # TODO: what is best n_fft and what is n_fft uberhaupt?
+    spectra = rimworld_utils.create_spectrum_dataset(data_folder, n_fft=2048)
+
+    metadata \
+        .join(spectra) \
+        .to_csv(f'data/generated/dataset_{data_type}.csv')
 
 
 if __name__ == "__main__":
-    create_test_dataset()
+    create_spectrum_dataset('test')
